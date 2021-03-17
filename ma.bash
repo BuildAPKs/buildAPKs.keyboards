0be9a3ea035a2fec33c9128b40d19aa607ae0f2c
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-17 19:55:03 -0400

    commit 421661631 on 20210317

--- ma.bash
+++ ma.bash
@@ -8,5 +8,7 @@ shopt -s nullglob globstar
 cd "$JDR"
 # _AT_ username/repository commit
 _AT_ rkkr/simple-keyboard 439a5e64bede9c65dae6ebafc9b8a67b6d353cb3
+_AT_ rretzbach/LearningKeyboard 0f3a2ab9d19e438737ff421a1d139367554a5056
 _AT_ sashomasho/bulkey d6b22ecf9989320506985da7382dbb0c29c15404
+_AT_ tillspeicher/TypologyAndroidApp 9970b2c2fd86b8e680ba13ab6060346461d78558
 # ma.bash OEF
