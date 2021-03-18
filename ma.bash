eb33a9debd58be3672a35f26233c4767669667fa
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-17 23:21:06 -0400

    commit 206168675 on 20210317

--- ma.bash
+++ ma.bash
@@ -7,8 +7,18 @@ shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
+_AT_ Riateche/fast12-keyboard 14f2f6a4a3c5c1759433b6d64e2d5d2a776d2d61
+_AT_ Riateche/twotaps-keyboard 26ecb63881b3a5b0675208a60f850b54581a2406
+_AT_ Youhana-Hana/MultiLingualKeyboard b898c1a7c4870d57ff5ebac52abc6917ab017f9d
+_AT_ blackyblack/GraphKeyboard 4787e4570c8128f4ce406fd87fbde6ac0c191661
+_AT_ caminante99/ChordBoard 1e67d6be9ae27ac660c4f4fdea7a8f98fa76d682
+_AT_ emergentdotorg/morseime 578b0b0becd5d0a801862b9140e20591d87299f6
+_AT_ dhaval0122/keyboard_sample_2 7b399decb5a75bd417e41f5c64f5504ddff8a7a1
+_AT_ dhaval0122/keyboard_sample_2 7b399decb5a75bd417e41f5c64f5504ddff8a7a1
 _AT_ rkkr/simple-keyboard 439a5e64bede9c65dae6ebafc9b8a67b6d353cb3
-_AT_ rretzbach/LearningKeyboard 0f3a2ab9d19e438737ff421a1d139367554a5056
+_AT_ wanleung/Stroke5Keyboard-android 33b409d56c15852a85d5f4d631c38c7e698ab4e3
+_AT_ retrohacker/CS202-Final-Project a9825c39fb10b54e0bc014d5e145a3c6fb703cd9
+_AT_ nschwermann/Math-Keyboard 5a64bcebce2848206164d5029617d4776e217014
 _AT_ sashomasho/bulkey d6b22ecf9989320506985da7382dbb0c29c15404
 _AT_ tillspeicher/TypologyAndroidApp 9970b2c2fd86b8e680ba13ab6060346461d78558
 # ma.bash OEF
